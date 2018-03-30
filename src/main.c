f773561bae1bef05d1f40a0c93b7ccff8dce0c10
Liu Qun (liuqun) <qunliu@zyhx-group.com>
2018-03-30 17:47:49 +0800

    上传一份libtss2-tcti-mssim.so测试模板

--- src/main.c
+++ src/main.c
@@ -1,11 +1,120 @@
+#include <dlfcn.h>
+#include <stdlib.h>
+#include <string.h>
 #include "tool.h"
+#include <stdio.h>
+
+typedef enum {
+    OPTION_DEFAULT=0,
+    OPTION_MSSIM=0,
+    OPTION_DEVICE=1,
+    OPTION_TABRMD=2,
+} option_t;
+
+void foobar(option_t opt)
+{
+const struct {
+    char *lib_name;
+    char *func_symbol;
+    char *default_opts;
+} KNOWN_TCTI_LIST[] = {
+    {
+        .lib_name="libtss2-tcti-mssim.so",
+        .func_symbol="Tss2_Tcti_Mssim_Init",
+        .default_opts="tcp://127.0.0.1:2321",
+    },
+    {
+        .lib_name="libtss2-tcti-device.so",
+        .func_symbol="Tss2_Tcti_Device_Init",
+        .default_opts="/dev/tpm0",
+    },
+    {
+        .lib_name="libtss2-tcti-tabrmd.so",
+        .func_symbol="Tss2_Tcti_Tabrmd_Init",
+        .default_opts="bus_type=system,bus_name=com.intel.tss2.Tabrmd",
+    },
+};
+    const char *tcti_lib_name = KNOWN_TCTI_LIST[opt].lib_name;
+    const char *tcti_func_symbol = KNOWN_TCTI_LIST[opt].func_symbol;
+    const char *tcti_opts = KNOWN_TCTI_LIST[opt].default_opts;
+    TSS2_TCTI_CONTEXT *tcti_ctx = NULL;
+    void *dll_handle;
+    dll_handle = dlopen(tcti_lib_name, RTLD_NOW);
+//printf("dll_handle = %p\n", dll_handle);
+    if (!dll_handle) {
+        return;  // TODO: Report an error.
+    }
+    TSS2_TCTI_INIT_FUNC Tss2_Tcti_Initialize = NULL;
+    Tss2_Tcti_Initialize = (TSS2_TCTI_INIT_FUNC) dlsym(dll_handle, tcti_func_symbol);
+    size_t size;
+    TSS2_RC err;
+    size = 0;
+    err = Tss2_Tcti_Initialize(NULL, &size, tcti_opts);
+    if (err) {
+        goto CLEANUP_TCTI_CTX;
+    }
+    tcti_ctx = malloc(size);
+    memset(tcti_ctx, 0x00, size);
+    err = Tss2_Tcti_Initialize(tcti_ctx, &size, tcti_opts);
+    if (err) {
+        goto CLEANUP_TCTI_CTX;
+    }
+    Tss2_Tcti_Finalize(tcti_ctx);
+CLEANUP_TCTI_CTX:
+    if (dll_handle) {
+        dlclose(dll_handle);
+    }
+    if (tcti_ctx) {
+        free(tcti_ctx);
+        tcti_ctx = NULL;
+    }
+    return;
+}
+
 int main()
 {
-    int err;
-    err = tool_init();
-    if (err)
-    {
-        return (255);
-    }
+    foobar(OPTION_MSSIM);
     return (0);
+}
+
+int fun()
+{
+    TSS2_RC err;
+    size_t size;
+    TSS2_SYS_CONTEXT *sys_ctx = NULL;
+    TSS2_TCTI_CONTEXT *tcti_ctx = NULL;
+    TSS2_ABI_VERSION any;
+    memset(&any, 0x00, sizeof(TSS2_ABI_VERSION));
+    guess_tss2_abi_version(&any);
+    size = Tss2_Sys_GetContextSize(0);
+    sys_ctx = malloc(size);
+    memset(sys_ctx, 0x00, size);
+    err = Tss2_Sys_Initialize(sys_ctx, size, tcti_ctx, &any);
+    switch (err) {
+        case TSS2_RC_SUCCESS:
+            break;
+        case TSS2_SYS_RC_BAD_REFERENCE:
+            break;
+        case TSS2_SYS_RC_ABI_MISMATCH:
+            // Try with NULL to suppress ABI version check.
+            err = Tss2_Sys_Initialize(sys_ctx, size, tcti_ctx, NULL);
+            break;
+        default:
+            break;
+    }
+    if (err) {
+        goto CLEANUP;
+    }
+    Tss2_Sys_Finalize(sys_ctx);
+    //Tss2_Tcti_Finalize(tcti_ctx);
+CLEANUP:
+    if (sys_ctx) {
+        free(sys_ctx);
+        sys_ctx = NULL;
+    }
+    if (tcti_ctx) {
+        free(tcti_ctx);
+        tcti_ctx = NULL;
+    }
+    return ((int) err);
 }
--- src/tool.c
+++ src/tool.c
@@ -1,6 +1,31 @@
 #include "tool.h"
-const int TOOL_INIT_SUCCESS = 0;
-int tool_init()
-{
-	return (TOOL_INIT_SUCCESS);
+
+/** Guess TSS2_ABI_VERSION numbers
+ *
+ * ABI version checking method was changed by upstream developers. Here we are
+ * trying to guess out the hidden version numbers in the master branch of
+ * tpm2-tss. In the future, we may pull those magic version numbers directly
+ * from "Tss2_Sys_Initialize.c" of upstream source tree. See:
+ * https://github.com/tpm2-software/tpm2-tss/pull/864
+ */
+const TSS2_ABI_VERSION guess_tss2_abi_version(TSS2_ABI_VERSION *answer) {
+#ifndef TSSWG_INTEROP
+    const uint32_t TSSWG_INTEROP = 1;
+#endif
+#ifndef TSS_SAPI_FIRST_FAMILY
+    const uint32_t TSS_SAPI_FIRST_FAMILY = 2;
+#endif
+#ifndef TSS_SAPI_FIRST_LEVEL
+    const uint32_t TSS_SAPI_FIRST_LEVEL = 1;
+#endif
+#ifndef TSS_SAPI_FIRST_VERSION
+    const uint32_t TSS_SAPI_FIRST_VERSION = 108;
+#endif
+
+    answer->tssCreator = TSSWG_INTEROP;
+    answer->tssFamily = TSS_SAPI_FIRST_FAMILY;
+    answer->tssLevel = TSS_SAPI_FIRST_LEVEL;
+    answer->tssVersion = TSS_SAPI_FIRST_VERSION;
+
+    return (*answer);
 }
